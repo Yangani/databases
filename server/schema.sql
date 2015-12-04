c79b081ed911c2df7d17c136f40eb7e5cab81316
MKS-students <mks.students@hackreactor.com>
2015-12-04 17:33:07 -0600

    database and server running

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,23 @@
+
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `user_id` VARCHAR(15) NULL DEFAULT NULL,
+  `roomname` VARCHAR(15) NULL DEFAULT 'lobby',
+  `text` VARCHAR(144) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+CREATE TABLE users (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(15) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
